9d7a6b05cc40931390b6570bc9c781aa31f9c739
sanghyeok park <feel6404@gmail.com>
2024-11-19 00:41:12 +0900

    test4

--- components/ThreadsAuthButton.js
+++ components/ThreadsAuthButton.js
@@ -2,18 +2,18 @@ import styles from './ThreadsAuthButton.module.css'
 
 export default function ThreadsAuthButton() {
     const handleThreadsAuth = () => {
-      const AUTH_URL = 'https://threads.net/oauth/authorize'
-      const params = {
-        client_id: process.env.NEXT_PUBLIC_THREADS_CLIENT_ID,
-        redirect_uri: process.env.NEXT_PUBLIC_REDIRECT_URI,
-        scope: 'threads_basic,threads_content_publish,threads_read_replies,threads_manage_replies,threads_manage_insights',
-        response_type: 'code'
-      }
-  
-      const queryString = new URLSearchParams(params).toString()
-      window.location.href = `${AUTH_URL}?${queryString}`
+        const AUTH_URL = 'https://www.threads.net/oauth/authorize'  // www. 추가
+        const params = {
+          client_id: process.env.NEXT_PUBLIC_THREADS_CLIENT_ID,
+          redirect_uri: process.env.NEXT_PUBLIC_REDIRECT_URI,
+          scope: 'threads_basic,threads_content_publish,threads_read_replies,threads_manage_replies,threads_manage_insights',
+          response_type: 'code',
+          state: 'random_state_string'  // CSRF 방지를 위한 state 추가
+        }
+      
+        const queryString = new URLSearchParams(params).toString()
+        window.location.href = `${AUTH_URL}?${queryString}`
     }
-  
     return (
         <button className={styles.button} onClick={handleThreadsAuth}>  
             Threads ID로 인증하기
